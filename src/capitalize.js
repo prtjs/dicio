0e765a2b61f3f6593821a0459fd5a4128156acd4
Matheus Alves <theuves@gmail.com>
2019-02-14 10:54:50 -0300

    Remove capitalize.js

--- src/capitalize.js
+++
@@ -1,9 +0,0 @@
-const capitalizeWord = word => word.charAt(0).toUpperCase() + word.substring(1);
-
-const capitalize = string =>
-  string
-    .split(' ')
-    .map(word => (word.length > 2 ? capitalizeWord(word) : word))
-    .join(' ');
-
-module.exports = capitalize;
